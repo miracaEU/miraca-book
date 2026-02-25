e9cf0622728381cd48c1a71a64fecd84e283399c
claudia-u <c.urricelqui@gmail.com>
2026-02-25 16:46:56 +0100

    moved UC2

---
+++ miraca-book/usecases/UC2/src/miraca_template/example.py
@@ -0,0 +1,17 @@
+def example_function(x, y):
+    """Example function that demonstrates how to format the docstring
+
+    Also used in tests.
+    Parameters
+    ----------
+    x : int
+        Description of parameter `x`.
+    y :
+        Description of parameter `y` (with type not specified).
+
+    Returns
+    -------
+    out : int
+          Description of the output.
+    """
+    return x + y
---
+++ miraca-book/usecases/UC2/tests/test_example.py
@@ -0,0 +1,5 @@
+from miraca_template.example import example_function
+
+
+def test_example_function():
+    assert example_function(1, 2) == 3
